445ee649e8ec974bc20926ea6e498e016c17162e
Dany CORBINEAU <danycor2@hotmail.fr>
2020-02-23 16:12:23 +0100

    [#02] Liste des containers - Service Docker

--- Server/nest-api/src/app.controller.ts
+++ Server/nest-api/src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+const DockerService = require('./DockerService');
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
