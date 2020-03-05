ff3082030b10bf47cd7ce03b5ef57d8e83e36d27
DanyCorbineau <danycor2@hotmail.fr>
2020-03-05 15:14:13 +0100

    [#03] ADD server api create script

--- Client/app/services/script.js
+++ Client/app/services/script.js
@@ -3,8 +3,7 @@ import Service, {inject as service} from '@ember/service';
 export default class ScriptService extends Service {
   @service store;
   create(script) {
-    let newScript = this.store.createRecord('script',  script);
-    newScript.save();
-    console.log('Save script : '+script )
+    this.store.createRecord('script',  script).save();
+    console.log(script)
   }
 }
