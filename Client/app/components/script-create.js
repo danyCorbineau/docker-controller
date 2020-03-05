e2e11ba0ef3444b4646863b9949aca9bc410473b
Nicholas Stock <nick@gembani.com>
2020-03-05 15:23:10 +0100

    found souldution https://github.com/frank06/ember-octane-blog

--- Client/app/components/script-create.js
+++ Client/app/components/script-create.js
@@ -2,16 +2,11 @@ import Component from '@glimmer/component';
 import {tracked} from "@glimmer/tracking";
 import {action} from '@ember/object';
 import { inject as service } from '@ember/service';
-
+import { get } from '@ember/object';
 export default class ScriptCreateComponent extends Component {
-  @tracked scriptExt = "";
-  @tracked scriptName = "";
-  @tracked scriptContent = "";
+  
 
-  @service script;
-
-  @action submitCreate() {
-    let newScript = {content: this.scriptContent, extension: this.scriptExt, name: this.scriptName};
-    this.script.create(newScript);
+  @action submitCreate(new_script) {
+    new_script.save()
   }
 }
--- Client/app/routes/home.js
+++ Client/app/routes/home.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default class HomeRoute extends Route {
+  model(){
+    return this.store.createRecord('script');
+  }
 }
