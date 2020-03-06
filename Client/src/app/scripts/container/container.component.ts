abff672662cf543e96b6528efbb9fd0dddd2d82e
BAPTISTE LECHAT <baptiste.lechat@ynov.com>
2020-03-06 16:51:56 +0100

    [#02] Lister containers - Afficher containers dans popup

---
+++ Client/src/app/scripts/container/container.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Container } from '../../model/container';
+
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent implements OnInit {
+  container: Container;
+
+  constructor(
+    public dialogRef: MatDialogRef<ContainerComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Container) { 
+      this.container = data;
+      console.log(data);
+    }
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
