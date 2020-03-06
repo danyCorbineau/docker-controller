6bdd08478a3681d76361a6c950cb69d0e011f5f5
BAPTISTE LECHAT <baptiste.lechat@ynov.com>
2020-03-06 14:04:07 +0100

    [#02] Lister containers - Ajout tableau statique

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CreateListComponent } from './scripts/create-list/create-list.component';
+import { ContainerListComponent } from './scripts/container-list/container-list.component';
 import { ScriptsComponent } from './scripts/scripts.component';
-import { HttpClientModule } from '@angular/common/http';
 
 // materielAngular
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CreateListService } from './services/create-list.service';
+// import {MatSort} from '@angular/material/sort';
+// import { MatTableDataSource } from '@angular/material/table';
+import {MatTableModule} from '@angular/material/table';
+
 import {MatButtonModule} from '@angular/material/button';
 
 
@@ -21,7 +28,8 @@ import {MatButtonModule} from '@angular/material/button';
     AppComponent,
     HomeComponent,
     CreateListComponent,
-    ScriptsComponent
+    ScriptsComponent,
+    ContainerListComponent
   ],
   imports: [
     BrowserModule,
@@ -30,6 +38,10 @@ import {MatButtonModule} from '@angular/material/button';
     MatInputModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
+    // MatSort,
+    // MatTableDataSource,
+    MatTableModule,
     ReactiveFormsModule,
     MatButtonModule
 
