ff43139795d2b7e2136d63eea48456cbf317095f
DanyCorbineau <danycor2@hotmail.fr>
2020-03-13 08:22:54 +0100

    [#03] Lister container - change getall

---
+++ Server/nest-api/src/container/container.model.ts
@@ -0,0 +1,11 @@
+
+export class ContainerModel {
+    unique_id: string;
+    status: string;
+    scripts: any;
+    names: string[];
+    image: string;
+    state: string;
+    ports: string[];
+    created: number;
+}
--- Server/nest-api/src/container/container.service.ts
+++ Server/nest-api/src/container/container.service.ts
@@ -1,6 +1,8 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { Model } from 'mongoose';
-import { Container } from './container.interface';
+import {Container} from "./container.interface";
+import {ContainerModel} from "./container.model";
+
 
 @Injectable()
 export class ContainerService {
@@ -9,19 +11,14 @@ export class ContainerService {
         private readonly containerModel: Model<Container>,
     ) {}
 
-    async findAll(): Promise<Container[]> {
+    async findAll(): Promise<ContainerModel[]> {
         // return this.containerModel.find().exec();
         return [
-            // tslint:disable-next-line: max-line-length
-            {unique_id: '1220', names: ['abc'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...'},
-            // tslint:disable-next-line: max-line-length
-            {unique_id: '456', names: ['def'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...'},
-            // tslint:disable-next-line: max-line-length
-            {unique_id: '789', names: ['ghi'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...'},
-            // tslint:disable-next-line: max-line-length
-            {unique_id: '101', names: ['klm'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...'},
-            // tslint:disable-next-line: max-line-length
-            {unique_id: '112', names: ['nop'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...'},
+            {unique_id: '1220', names: ['abc'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...', scripts: {}},
+            {unique_id: '456', names: ['def'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...', scripts: {}},
+            {unique_id: '789', names: ['ghi'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...', scripts: {}},
+            {unique_id: '101', names: ['klm'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...', scripts: {}},
+            {unique_id: '112', names: ['nop'], image: 'img', state: 'running', ports: ['123:567', '789:1011'], created: 1583508175, status: 'looking for ...', scripts: {}},
           ];
     }
 
