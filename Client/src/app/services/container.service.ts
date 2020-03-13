dde2e1353011db4c2d90cbf5963e14f8f74acde5
DanyCorbineau <danycor2@hotmail.fr>
2020-03-13 16:30:23 +0100

    [#03] Lister container - fix deserialiser

--- Client/src/app/services/container.service.ts
+++ Client/src/app/services/container.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Container} from "../model/container";
 import {ApiService} from "./api.service";
-// @ts-ignore
-import { deserialize } from 'json-api-deserialize';
+import { deserialize } from 'deserializer';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +9,7 @@ import { deserialize } from 'json-api-deserialize';
 export class ContainerService extends ApiService<Container> {
   API_URL = 'http://localhost:3000';
   entity_endpoint =  'containers';
-  async getContainers(): Promise<Container[]>
+  async getContainers(): Promise<any>
   {
     let containerObjectsJsonAPI:any = await new Promise(r => {let obs = this.getAll().subscribe(data => {obs.unsubscribe(); r(data)})});
     return deserialize(containerObjectsJsonAPI).data;
