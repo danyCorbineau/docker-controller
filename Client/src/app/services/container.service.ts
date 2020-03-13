5156b840098c3be3d9865f0c7499d7c2f166a31d
DanyCorbineau <danycor2@hotmail.fr>
2020-03-13 11:19:13 +0100

    [#03] Lister container - link front-back

--- Client/src/app/services/container.service.ts
+++ Client/src/app/services/container.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import {Container} from "../model/container";
-import {Service, DocumentCollection, Resource} from 'ngx-jsonapi';
-
+import {ApiService} from "./api.service";
+// @ts-ignore
+import { deserialize } from 'json-api-deserialize';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ContainerService extends Service {
-
-  public resource = Container;
-  public type = 'containers';
-  constructor() {
-    super();
-    this.register();
+export class ContainerService extends ApiService<Container> {
+  API_URL = 'http://localhost:3000';
+  entity_endpoint =  'containers';
+  async getContainers(): Promise<Container[]>
+  {
+    let containerObjectsJsonAPI:any = await new Promise(r => {let obs = this.getAll().subscribe(data => {obs.unsubscribe(); r(data)})});
+    return deserialize(containerObjectsJsonAPI).data;
   }
 
 }
