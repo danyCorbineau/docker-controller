58c23c5bd88ec1298b6d2aea70297e867475d52a
Dany CORBINEAU <danycor2@hotmail.fr>
2020-03-19 11:37:07 +0100

    ADD server port .env

--- Server/nest-api/src/app.module.ts
+++ Server/nest-api/src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScriptModule } from './script/script.module';
 import { ContainerController } from './container/container.controller';
 import { ContainerModule } from './container/container.module';
 
+let envName = '.env';
+console.log('env : ', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'test') {
+  envName = '.env.test';
+}
+
 @Module({
-  imports: [ScriptModule, ContainerModule],
+  imports: [
+    ScriptModule, 
+    ContainerModule,
+    ConfigModule.forRoot({
+      envFilePath: envName
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: []
